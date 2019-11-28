665989144a5706736e50199bc061c4415d5ebe94
Marciano <steelplate@gmail.com>
2019-11-29 00:45:57 +0100

    Update test-db.php

--- test-db.php
+++ test-db.php
@@ -1,8 +1,11 @@
 <?php
-$link = mysql_connect('sentia-ta-db.mysql.database.azure.com', 'db_admin1@Sentia-TA-DB', 'Sub.123As>!3dfa');
-if (!$link) {
-die('Could not connect: ' . mysql_error());
-}
-echo 'Connected successfully';
-mysql_close($link);
-?>
+     //PATH TO YOUR FILE
+     require_once('wp-config.php');
+     //using wp-config variables 
+     $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+     if (!$link) {
+          die('Could not connect: ' . mysql_error());
+     }
+     echo 'Connected successfully';
+     mysql_close($link);
+     ?>
