f04dc8055a1b9977497b7070a1af06dd584606de
Marciano <steelplate@gmail.com>
2019-11-29 00:44:46 +0100

    Update test-db.php

--- test-db.php
+++ test-db.php
@@ -1,20 +1,5 @@
 <?php
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
-foreach ($_SERVER as $key => $value) {
- if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
- continue;
- }
- 
- $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
- $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
- $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
- $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-
-$link = mysql_connect('$connectstr_dbhost', '$connectstr_dbusername', '$connectstr_dbpassword');
+$link = mysql_connect('sentia-ta-db.mysql.database.azure.com', 'db_admin1@Sentia-TA-DB', 'Sub.123As>!3dfa');
 if (!$link) {
 die('Could not connect: ' . mysql_error());
 }
