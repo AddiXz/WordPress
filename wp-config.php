73c5e0d9735708388d00dc36539bdbc60e1842e8
Marciano <steelplate@gmail.com>
2019-11-29 00:13:35 +0100

    Update and rename wp-config-sample.php to wp-config.php
    Also added DB pull info for azure connection

--- wp-config.php
+++ wp-config.php
@@ -19,17 +19,34 @@
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+
+foreach ($_SERVER as $key => $value) {
+ if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+ continue;
+ }
+ 
+ $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+// ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define('DB_NAME', $connectstr_dbname);
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define('DB_USER', $connectstr_dbusername);
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define('DB_PASSWORD', $connectstr_dbpassword);
 
-/** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+/** MySQL hostname : this contains the port number in this format host:port . Port is not 3306 when using this feature*/
+define('DB_HOST', $connectstr_dbhost);
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
