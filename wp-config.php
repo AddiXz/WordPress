874e14f1a0815c1eeff078db10558a8c484490e4
Marciano <steelplate@gmail.com>
2019-11-30 07:06:42 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -18,41 +18,34 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
+//Using environment variables for DB connection information
 $connectstr_dbhost = '';
 $connectstr_dbname = '';
 $connectstr_dbusername = '';
 $connectstr_dbpassword = '';
-
 foreach ($_SERVER as $key => $value) {
- if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
- continue;
- }
- 
- $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
- $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
- $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
- $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+    if (strpos($key, "MYSQLCONNSTR_") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
-
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', $connectstr_dbname);
-
 /** MySQL database username */
 define('DB_USER', $connectstr_dbusername);
-
 /** MySQL database password */
 define('DB_PASSWORD', $connectstr_dbpassword);
-
-/** MySQL hostname : this contains the port number in this format host:port . Port is not 3306 when using this feature*/
+/** MySQL hostname */
 define('DB_HOST', $connectstr_dbhost);
-
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
-
+define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '');
 
 /**#@+
  * Authentication Unique Keys and Salts.
